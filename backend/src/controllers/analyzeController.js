c1cd4e1f0e51a180f53abb2339d324957e50a24f
Joe Abraham <joeabraham767@gmail.com>
2026-06-07 16:15:40 +0530

    Day-6 carrier recommendation engine

--- backend/src/controllers/analyzeController.js
+++ backend/src/controllers/analyzeController.js
@@ -4,16 +4,25 @@ const analyzeProject = (req, res) => {
 
   let skills = [];
 
+  let carrierSuggestion=[];
+
   if (githubUrl.toLowerCase().includes("react")) {
     skills.push("React");
+    carrierSuggestion.push("Frontend Developer");
   }
 
   if (githubUrl.toLowerCase().includes("node")) {
     skills.push("Node.js");
+    carrierSuggestion.push("Backend Developer");
   }
 
   if (githubUrl.toLowerCase().includes("python")) {
     skills.push("Python");
+    carrierSuggestion.push("Python Developer");
+  }
+
+  if( skills.includes("React") && skills.includes("Node.js")){
+    carrierSuggestion.push("Full Stack Developer");
   }
 
   if (skills.length === 0) {
@@ -25,7 +34,8 @@ const analyzeProject = (req, res) => {
   res.json({
     success: true,
     receivedUrl: githubUrl,
-    skills: skills
+    skills: skills,
+    carrierSuggestion: carrierSuggestion
   });
 
 };
